96e45f297e1d8b0d3f924180cc39ca847293af1e
bollu <siddu.druid@gmail.com>
2016-08-25 02:32:03 +0530

    added tokenization into shell

---
+++ os-assign-2/shell.c
@@ -0,0 +1,269 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
+
+/* *** Context *** */
+static const int MAX_CWD_LENGTH = 1024 * 20;
+static const int MAX_USERNAME_LENGTH = 1024 * 20;
+static const int MAX_HOSTNAME_LENGTH = 1024 * 20;
+typedef struct  {
+    char cwd[MAX_CWD_LENGTH];
+    char username[MAX_USERNAME_LENGTH];
+    char hostname[MAX_HOSTNAME_LENGTH];
+
+} Context;
+
+Context *context_new();
+void context_update(Context *context);
+int context_should_quit(const Context *ctx);
+
+
+/* *** Command *** */
+typedef enum {
+    COMMAND_TYPE_NONE,
+    COMMAND_TYPE_CD,
+    COMMAND_TYPE_PWD,
+    COMMAND_TYPE_EXIT,
+    COMMAND_TYPE_LAUNCH,
+
+} CommandType;
+typedef struct Command {
+    CommandType type;
+    char **args;
+    struct Command *next;
+} Command;
+
+Command* command_new(CommandType type, char **args) {
+    Command *command = (Command*)malloc(sizeof(Command));
+    command->next = NULL;
+    command->type = type;
+    command->args = args;
+
+    return command;
+}
+
+/* *** Context Impl *** */
+Context *context_new() {
+    Context *ctx = (Context*)malloc(sizeof(Context));
+    return ctx;
+}
+
+void context_update(Context *context) {
+    getcwd(context->cwd, MAX_CWD_LENGTH); 
+    gethostname(context->hostname, MAX_HOSTNAME_LENGTH);
+    strncpy(context->username, getlogin(), MAX_USERNAME_LENGTH);
+}
+
+int context_should_quit(const Context *ctx){
+    return 0;
+}
+
+/* *** Command Implementation *** */
+
+/* *** REPL Implementation *** */
+
+void repl_print_prompt(const Context *ctx) {
+    printf("\n%s@%s:%s>", ctx->cwd, ctx->username, ctx->hostname);
+}
+
+
+char* read_single_lne() {
+    static const int BUFFER_BLOCK_SIZE = 1024;
+    int buffer_block_multiple = 1;
+
+    char *output = (char*)malloc(sizeof(char) * BUFFER_BLOCK_SIZE);
+    int buffer_pos = 0;
+
+    while(1) {
+        int c = getchar();
+        if (c == '\n' || c == '\0' || c == EOF) {
+            break;
+        } else {
+            if (buffer_pos == buffer_block_multiple * BUFFER_BLOCK_SIZE) {
+                buffer_block_multiple++;
+                output = realloc(output, 
+                        sizeof(char) * BUFFER_BLOCK_SIZE * buffer_block_multiple);
+            }
+
+            output[buffer_pos++] = c;
+            
+        }
+    }
+
+    output[buffer_pos] = '\0';
+    return output;
+
+}
+
+typedef enum {
+    TOKEN_TYPE_SEMICOLON,
+    TOKEN_TYPE_WORD,
+} TokenType;
+
+typedef struct Token {
+    TokenType type;
+    char *string;
+    struct Token *next;
+} Token;
+
+Token* token_new_semicolon() {
+    Token *t = (Token*)malloc(sizeof(Token));
+    t->type = TOKEN_TYPE_SEMICOLON;
+    t->string = NULL;
+    t->next = NULL;
+    return t;
+}
+
+Token* token_new_word(char *word) {
+    assert(word != NULL);
+    Token *t = (Token*)malloc(sizeof(Token));
+    t->type = TOKEN_TYPE_WORD;
+    t->string = word;
+    t->next = NULL;
+    return t;
+}
+
+void token_print(Token t) {
+    if (t.type == TOKEN_TYPE_SEMICOLON) {
+        printf("t[;]");
+    }
+    else if (t.type == TOKEN_TYPE_WORD) {
+        printf("t[%s]", t.string);
+    }
+}
+
+int is_char_whitespace(char c) {
+    return c == ' ' || c == '\n' || c == '\t';
+}
+
+Token* tokenize_line(char *line) {
+    Token *head = NULL;
+    Token *current = NULL;
+
+
+    int i = 0;
+    while(i < strlen(line)) {
+        while(i < strlen(line) && is_char_whitespace(line[i])) {
+            i++;
+        }
+
+        //if we have reached the end of the tokens, break
+        if (i == strlen(line)) {
+            break;
+        }
+
+        else if (line[i] == ';') {
+            i++;
+            Token *new = token_new_semicolon();
+            if (current) {
+                current->next = new;
+                current = current->next;
+            } else {
+                head = current = new;
+            }
+        }
+        else {
+            const int prev_pos = i;
+            while(i < strlen(line) && !is_char_whitespace(line[i]) && line[i] != ';') {
+                i++;
+            }
+
+            const int word_len = i - prev_pos;
+            char *word_buf = (char *)malloc(sizeof(char) * (word_len + 1));
+            //TODO: write a function that takes a slice out of it
+            for(int j = 0; j < word_len; j++) { word_buf[j] = line[prev_pos + j]; }
+            word_buf[word_len] = '\0';
+
+            Token *new = token_new_word(word_buf);
+            if (current) {
+                current->next = new;
+                current = current->next;
+            } else {
+                head = current = new;
+            }
+        }
+    }
+    assert(head != NULL);
+    return head;
+    
+};
+
+
+/* Grammar of REPL syntax  (in EBNF)
+ * This can be parsed properly using recursive-descent, but let's strtok for now
+ * Expr := Command ; Expr | Command
+ * Command := <name> [args]*
+ */
+
+Command *parse_command(const Token *head) {
+    if (head == NULL) {
+        return NULL;
+    }
+
+    return NULL;
+
+};
+
+
+
+Command* repl_read(Context *ctx){
+
+    repl_print_prompt(ctx);
+    char *line = read_single_lne();
+    
+    Token *tokens = tokenize_line(line);
+
+    for(Token *t = tokens; t != NULL; t = t->next) {
+        token_print(*t);
+    }
+
+    Command *commands = NULL;
+    return commands;
+
+};
+
+
+int repl_launch(char **args) {
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        // Child process
+        if (execvp(args[0], args) == -1) {
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        // Error forking
+        perror("lsh");
+    } else {
+        // Parent process
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
+void repl_eval(Command *command) {
+    if (command->type == COMMAND_TYPE_LAUNCH) {
+        repl_launch(command->args);
+    }
+}
+
+int main(int argc, char **argv) {
+    Context *ctx = context_new();
+
+    while(!context_should_quit(ctx)) {
+        context_update(ctx);
+        Command *commands =  repl_read(ctx);
+        for(Command *c = commands; c != NULL; c = c->next) {
+            repl_eval(c);
+        }
+    }
+    return 0;
+}
