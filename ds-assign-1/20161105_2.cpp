6ca8b8f98334a0613d031eb6c7a7134214399532
Siddharth Bhat <siddu.druid@gmail.com>
2020-01-24 01:18:00 +0530

    add link to bellman ford

---
+++ ds-assign-1/20161105_2.cpp
@@ -0,0 +1,117 @@
+// https://www.cs.jhu.edu/~mdinitz/classes/IntroAlgorithms/Fall2014/Lectures/lecture13.pdf
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include "mpi.h"
+using namespace std;
+using I = int;
+I V, E;
+I *dist;
+I *adj;
+int rnk, nrnk;
+
+#define RNK cout<<"\033[31m["<<rnk<<"/" << nrnk << "]\033[0m" << "{" << __LINE__ << "}"
+
+void pr(I *a, I l, I r) { RNK; 
+  cout<<"(size:"<<r-l+1<<")";
+  for(int i = l; i <= r; ++i) cout <<a[i] <<" ";
+  cout<<"\n";
+}
+
+int log2floor(int n) { if (n <= 1) return 0; return 1 + log2floor(n/2); }
+
+I partition(I *a, I l, I r) {
+  assert(l <= r);
+  assert(l >= 0);
+  const I part = a[r];
+  I ltix = l-1; I geix = r;
+
+  while(ltix+1 < geix && geix-1 > ltix) {
+    // invariant
+    assert(a[geix] >= part); if(ltix>=l) { assert(a[ltix] < part); }
+
+    if (a[ltix+1] >= part) {
+      I t=a[ltix+1]; a[ltix+1] = a[geix-1]; a[geix-1]=t;
+      geix--;
+    } else { ltix++; }
+  }
+  assert(geix-1 == ltix);
+  a[r] = a[geix]; a[geix] = part;
+  return geix;
+}
+
+void qs_serial(I *a, I l, I r) {
+  RNK << "[" << l << "," << r << "]\n";
+  assert(l >= 0);
+  assert(l <= r);
+  I mid = partition(a, l, r);
+  assert(l <= mid);
+  assert(mid <= r);
+
+  if (l <= mid - 1) qs_serial(a, l, mid-1);
+  if (mid+1 <= r) qs_serial(a, mid+1, r);
+}
+
+int main( int argc, char **argv ) {
+
+  assert(argc == 3);
+  /* write your code here */
+
+  /* start up MPI */
+  MPI_Init( &argc, &argv );
+
+  MPI_Comm_rank( MPI_COMM_WORLD, &rnk );
+  MPI_Comm_size( MPI_COMM_WORLD, &nrnk );
+
+
+  /*synchronize all processes*/
+  MPI_Barrier( MPI_COMM_WORLD );
+  double tbeg = MPI_Wtime();
+
+  // our code starts here
+  // ====================
+
+  RNK << "XX\n";
+
+  if (rnk == 0) {
+    // 1. recieve input if leader
+    ifstream f(argv[1]);
+    f >> V >> E;
+    adj = (I*)calloc((V +1) * (V + 1), sizeof(I));
+    dist = new I[(V +1)];
+    for (int i = 0; i < E; i ++) {
+      int u, v, w; f >> u >> v >> w;
+      adj[u*V + v] = w;
+    }
+
+    RNK << "V:"  << V << "|E: " << E;
+    cout << "\n";
+    for (int i = 1; i <= V; ++i) {
+      for (int j = 1; j <= V; ++j) { 
+        cout << adj[i*V+j] << " ";
+      }
+      cout << "\n";
+    }
+    cout << "\n";
+  }
+
+
+  if (rnk == 0) {
+    ofstream f(argv[2]);
+    f << "XXX\n";
+  }
+
+  MPI_Barrier( MPI_COMM_WORLD );
+  double elapsedTime = MPI_Wtime() - tbeg;
+  double maxTime;
+  MPI_Reduce(&elapsedTime, &maxTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+  if (rnk == 0) {
+    printf( "Total time (s): %f\n", maxTime );
+  }
+
+  MPI_Finalize();
+  return 0;
+}
