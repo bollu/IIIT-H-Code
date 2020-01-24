9cbac85ced581f2c5f66557d1800c2b9fc929f3d
Siddharth Bhat <siddu.druid@gmail.com>
2020-01-24 07:38:57 +0530

    add code that works for n=1

--- ds-assign-1/20161105_2.cpp
+++ ds-assign-1/20161105_2.cpp
@@ -10,10 +10,12 @@
 using namespace std;
 using I = long long int;
 static const I INF = 1e10L;
-I V, E;
+I V, E, ESize;
 I src;
-I *dist;
+I *dist[2];
 I *adj;
+
+I* Ecur;
 
 // I *pred;
 // I *next;
@@ -91,6 +93,7 @@ int main( int argc, char **argv ) {
     ifstream f(argv[1]);
     f >> V >> E;
 
+
     adj = (I*)calloc((V +1) * (V + 1), sizeof(I));
     edgesrc = (I*)calloc((E+1), sizeof(I));
     edgedest = (I*)calloc((E+1), sizeof(I));
@@ -103,12 +106,13 @@ int main( int argc, char **argv ) {
     f >> src;
 
 
-    dist = new I[(V +1)];
+    dist[0] = new I[(V +1)];
+    dist[1] = new I[(V +1)];
     for(int i = 0; i <= V; i++) {
-        dist[i] = INF;
+        dist[0][i] = INF;
         adj[i] = -42;
     }
-    dist[src] = 0;
+    dist[0][src] = 0;
     // pred = new I[(V +1)];
     // next = new I[(V +1)];
 
@@ -121,16 +125,82 @@ int main( int argc, char **argv ) {
       cout << "\n";
     }
     cout << "\n";
+
   }
 
+  // distribute information
+  MPI_Bcast(&V, 1,
+          MPI_Datatype MPI_LONG_LONG_INT,
+          /*root=*/0, MPI_COMM_WORLD);
+  MPI_Bcast(&E, 1,
+          MPI_Datatype MPI_LONG_LONG_INT,
+          /*root=*/0, MPI_COMM_WORLD);
+
+  const I e_cur_size = ceill((float)E / nrnk);
+  if (rnk != 0) {
+      Ecur = (I*)calloc((E), sizeof(I));
+      edgesrc = (I*)calloc((E+1), sizeof(I));
+      edgedest = (I*)calloc((E+1), sizeof(I));
+      edgewt = (I*)calloc((E+1), sizeof(I));
+      dist[0] = new I[(V +1)]; dist[1] = new I[(V +1)];
+  }
+
+  MPI_Bcast(&edgewt, ESize+1,
+          MPI_Datatype MPI_LONG_LONG_INT,
+          /*root=*/0, MPI_COMM_WORLD);
+  MPI_Bcast(&edgesrc, ESize+1,
+          MPI_Datatype MPI_LONG_LONG_INT,
+          /*root=*/0, MPI_COMM_WORLD);
+  MPI_Bcast(&edgedest, ESize+1,
+          MPI_Datatype MPI_LONG_LONG_INT,
+          /*root=*/0, MPI_COMM_WORLD);
+  MPI_Bcast(&dist[0], V+1,
+          MPI_Datatype MPI_LONG_LONG_INT,
+          /*root=*/0, MPI_COMM_WORLD);
+  MPI_Bcast(&dist[1], V+1,
+          MPI_Datatype MPI_LONG_LONG_INT,
+          /*root=*/0, MPI_COMM_WORLD);
+
+  // distribute work
+  int l = 0; int r = 0; 
+  if (rnk == 0) {
+      const int num_per_rank = E / nrnk;
+
+      
+      int curr = E;
+      for(int i = nrnk-1; i >= 1; --i) {
+          const int curl = curr - num_per_rank+1;
+          MPI_Send(&curl, 1, MPI_LONG_LONG_INT, /*dest=*/i, 
+                  0, MPI_COMM_WORLD);
+          MPI_Send(&curr, 1, MPI_LONG_LONG_INT, /*dest=*/i, 
+                  0, MPI_COMM_WORLD);
+          curr = curl - 1;
+      }
+      r = curr;
+      l = 1;
+  } else {
+    MPI_Recv(&l, 1, MPI_LONG_LONG_INT, /*parent=*/0, 0, 
+        MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    MPI_Recv(&r, 1, MPI_LONG_LONG_INT, /*parent=*/0, 0, 
+        MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    assert(l >= 1);
+    assert(l <= r);
+    assert(r <= E);
+  }
+
+  int distix = 0; // IMPORTANT: starts from 0!
   for (int i = 1; i <= V;  ++i) {
-      for(int j = 1; j <= E; ++j) {
-          if (dist[edgesrc[j]] + edgewt[j] < dist[edgedest[j]]) {
-              dist[edgedest[j]] = dist[edgesrc[j]] + edgewt[j];
-              // pred[j] = i;
-              // next[i] = j;
+      for(int j = l; j <= r; ++j) {
+          if (dist[distix][edgesrc[j]] + edgewt[j] < dist[distix][edgedest[j]]) {
+              dist[distix][edgedest[j]] = dist[distix][edgesrc[j]] + edgewt[j];
           }
       }
+
+      MPI_Allreduce(dist[distix], dist[distix^1], V+1, 
+              MPI_LONG_LONG_INT, MPI_MIN, MPI_COMM_WORLD);
+      distix ^= 1;
   }
 
 
@@ -138,9 +208,9 @@ int main( int argc, char **argv ) {
     ofstream f(argv[2]);
     vector<int> sortixs(V);
     std::iota(sortixs.begin(), sortixs.end(),1);
-    sort(sortixs.begin(), sortixs.end(), [&](int i,int j){return dist[i]<dist[j];});
+    sort(sortixs.begin(), sortixs.end(), [&](int i,int j){return dist[distix][i]<dist[distix][j];});
     for(int i = 0; i < V; ++i) {
-        f << sortixs[i] << " " << dist[sortixs[i]] << "\n";
+        f << sortixs[i] << " " << dist[distix][sortixs[i]] << "\n";
     }
   }
 
