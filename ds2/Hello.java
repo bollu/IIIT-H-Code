3fbcf0e0f8504df4c639bf236e7e152254a30d9f
Siddharth Bhat <siddu.druid@gmail.com>
2020-02-12 20:18:01 +0530

    trying to get java working. I have never gotten java working

--- ds2/Hello.java
+++ ds2/Hello.java
@@ -37,24 +37,9 @@
  */
 package example.hello;
 
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
-public class Client {
-
-  private Client() {}
-
-  public static void main(String[] args) {
-
-    String host = (args.length < 1) ? null : args[0];
-    try {
-      Registry registry = LocateRegistry.getRegistry(host);
-      Hello stub = (Hello) registry.lookup("Hello");
-      String response = stub.sayHello();
-      System.out.println("response: " + response);
-    } catch (Exception e) {
-      System.err.println("Client exception: " + e.toString());
-      e.printStackTrace();
-    }
-  }
+public interface Hello extends Remote {
+    String sayHello() throws RemoteException;
 }
