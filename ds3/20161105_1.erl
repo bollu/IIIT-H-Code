5cb0c4bd4397b3f4513c0c9baf22dab1e9cd8197
Siddharth Bhat <siddu.druid@gmail.com>
2020-03-09 18:02:18 +0530

    add code for processes

--- ds3/20161105_1.erl
+++ ds3/20161105_1.erl
@@ -1,7 +1,16 @@
 #!/usr/bin/env escript
+-import(lists,[nth/2]). 
 
 
-new_process (Total, N) -> io:format("Process ~p\n", [N]).
+% new_process (Total, N) -> io:format("Process ~p\n", [N]).
+new_process (Total, N) -> 
+    io:format("Process ~p spawned.\n", [N]),
+    receive {sendtok, P0, PIDs, Token, Sender} ->
+        io:format("Process ~p received token ~p from process ~p.\n", [N, Token, Sender]),
+        if N == Total-1 -> P0 ! {sendtok, P0, PIDs, Token, N}; % final process send back to process 0
+           true -> nth(N+1, PIDs) ! {sendtok, P0, PIDs, Token, N} % non final process sends to 
+        end
+    end.
 
 %1 % process 0 firsts sends then receives
 %1 new_process (Total, 0) -> done.
@@ -10,10 +19,12 @@ new_process (Total, N) -> io:format("Process ~p\n", [N]).
 %1 new_process (Total, N) -> 
 %1     receieve {T,Sender} -> io:format("Process ~p received token ~p from process ~p.\n", N, T, Sender) end.
 
-spawnN(Total, 0) -> [];
-spawnN(Total, N) -> X = spawn(fun() -> new_process(Total, N) end),
-                    Y = spawnN(Total, N-1),
-                    [X|Y].
+spawnN(Total, N) -> 
+    if Total == N -> [];
+       true -> X = spawn(fun() -> new_process(Total, N) end),
+                Y = spawnN(Total, N+1),
+                [X|Y]
+    end.
 
 main(Args) -> [IP, OP] = Args,
                {ok, IF} = file:open(IP, [read]),
@@ -24,8 +35,12 @@ main(Args) -> [IP, OP] = Args,
                io:format("IP: ~p | OP: ~p | INPUT: ~p | Nproc: ~p | Tok: ~p\n", 
                          [IP, OP, RawInput, Nproc, Tok]),
                % spawn(fun() -> new_process() end),
-               PIDs = spawnN(Nproc, Nproc),
-               io:format("PIDs: ~p\n", [PIDs]),
-               timer:sleep (1000),
+               PIDs = spawnN(Nproc, 1),
+               [P1|_] = PIDs,
+               P1 ! {sendtok, self(), PIDs, Tok, 0},
+               receive {sendtok, P0, PIDs, Token, Sender} ->
+                io:format("Process 0 received token ~p from process ~p.\n", [Token, Sender])
+               end,
+               timer:sleep (10),
                ok.
 
