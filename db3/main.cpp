f8a2b28d68f22ffb2029b14d445e7487d908e3df
Siddharth Bhat <siddu.druid@gmail.com>
2020-03-10 18:52:39 +0530

    get find and insert basic working

--- db3/main.cpp
+++ db3/main.cpp
@@ -10,7 +10,12 @@ using namespace std;
 
 const int TREE_N = 3;
 
-// child[i] < router[i] < child[i+1]
+
+
+
+// child[i] < router[i] (STRICTLY LESS THAN)
+// router[i] <= child[i+1]
+// child[i] < router[i] <= child[i+1]
 // router only exists _between_ two children.
 // C C C
 //  R R
@@ -46,27 +51,65 @@ struct btree {
     btree() { root = nullptr; }
 };
 
+void printnode(node *n, int depth=0) {
+    if (n == nullptr) return;
+    n->check();
+
+    for(int i = 0; i < depth; ++i) { cerr << " "; }
+
+    if (n->isleaf) {
+        cerr << n << "|LEAF: [";
+        for(int i = 0; i < n->vals.size(); ++i) {
+            cerr << n->vals[i] << " ";
+        }
+        cerr << "]\n";
+    } else {
+        cerr << n << "|INTERNAL: [";
+        for(int i = 0; i < n->routers.size(); ++i) {
+            cerr << "|C:" << n->children[i] << "|R:" << int(n->routers[i]);
+        }
+        cerr << "|C:" << n->children[n->children.size() - 1];
+        cerr << "]\n";
+
+        for(int i =0; i < n->children.size(); ++i) {
+            printnode(n->children[i], depth+1);
+        }
+    }
+}
+
 
 node *gotoleaf(int k, node *cur) {
+    cerr << "gotoleaf(" << k << "," << cur << ")\n";
+    assert(cur);
+    if (cur->isleaf) { return cur; }
+    assert(cur->children.size() > 0);
+
     for(int i = 0; i < cur->routers.size(); ++i) {
-        if (cur->routers[i] < k) { 
+        if (k < cur->routers[i]) {
             return gotoleaf(k, cur->children[i]);
         }
     }
-    return gotoleaf(k, cur->children[cur->children.size()  -1]);
+    return gotoleaf(k, cur->children[cur->children.size()-1]);
 }
 
-bool find(int k, btree b) {
+bool find(int k, btree &b) {
+    if (!b.root) { return false; }
+
+    assert(b.root);
     node *leaf = gotoleaf(k, b.root);
+    assert(leaf->isleaf);
+    cerr << "find(" << k << ") in leaf: " << leaf << "\n";
     
-    int i = 1;
-    while(i <= leaf->vals.size() && k > leaf->vals[i]) { i++; }
-    if (i > leaf->vals.size()) { return false; }
-    assert(k <= leaf->vals[i]);
-    return k == leaf->vals[i];
+    for(int i = 0; i < leaf->vals.size(); ++i) {
+        if (leaf->vals[i] == k) { return true; }
+        // since array is sorted, if our leaf value ever exceeds the key
+        // then quit.
+        if (leaf->vals[i] > k) { return false; }
+    }
+    return false;
 };
 
-void insert_recur(int x, btree b, node *n)  {
+void insert_recur(int x, btree &b, node *n)  {
     if (n->isleaf) {
         if (n->vals.size() < TREE_N) {
             // look for index such that leaf[kloc] <= x.
@@ -80,6 +123,7 @@ void insert_recur(int x, btree b, node *n)  {
             for(int i = n->vals.size() + 1; i >= kloc; i--) { n->vals[i] = n->vals[i-1]; }
             // write x into the correct location
             n->vals[kloc] = x;
+            n->check();
         } else {
             // we need to free up space to insert a node. amaze
             node *n2 = new node;
@@ -125,7 +169,9 @@ void insert_recur(int x, btree b, node *n)  {
             }
             p->children[childloc+1] = n2;
             p->routers[childloc] = n->vals[n->vals.size()-1];
-
+            p->check();
+            n->check();
+            n2->check();
         }
 
     } else {
@@ -166,53 +212,70 @@ void insert_recur(int x, btree b, node *n)  {
             n->children[0] = left;
             n->routers[0] = splitrouter;
             n->children[1] = right;
+            n->check();
+            right->check();
+            left->check();
         }
 
         assert (n->children.size() < TREE_N);
 
-
-        for (int i = 0; i < n->routers.size(); ++i) {
-            if (x < n->routers[i]) {
-                return insert_recur(x, b, n->children[i]);
+        for(int i = 0; i < n->routers.size(); ++i) {
+            if (x <= n->routers[i]) {
+                insert_recur(x, b, n->children[i]);
             }
         }
-        return insert_recur(x, b, n->children[n->children.size()-1]);
+
+
+        node *leaf = new node();
+        leaf->isleaf = true;
+        leaf->parent = n;
+        leaf->vals.push_back(x);
+        
+        assert(n->children.size() < TREE_N);
+
+        // C0:10 || ---
+        // C0:10 || R0: 20 || C1:20 || --
+        n->routers.push_back(x);
+        n->children.push_back(leaf);
+        n->check();
+        leaf->check();
     }
 }
 
 
-void insert(int x, btree b) {
+void insert(int x, btree &b) {
     if (b.root == nullptr) {
+        cerr << "b.root == nullptr\n";
         b.root = new node();
         b.root->isleaf = false;
         
         b.root->children.push_back(new node);
         b.root->children[0]->isleaf = true;
         b.root->children[0]->vals.push_back(x);
-        b.root->routers.push_back(x);
+        b.root->check();
     } else {
         insert_recur(x, b, b.root);
     }
 };
 
-int count(int x, btree b) {};
-int range(int x, int y, btree b) {};
+int count(int x, btree &b) {};
+int range(int x, int y, btree &b) {};
+
 
 int main(int argc, char *argv[]) {
     assert(argc == 2);
-    cout << "reading: " << argv[1] << "\n";
+    cerr << "reading: " << argv[1] << "\n";
     ifstream f; f.open(argv[1], std::ifstream::in);
 
     btree b;
     std::string l;
     while (getline(f, l)) {
-        cout << "str: " << l << "\n";
         std::stringstream ss(l);
 
         string c; ss >> c;
-        cout << "str: " << c << "\n";
         if (c == "INSERT") {
             int x; ss >> x; insert(x, b);
+            assert(b.root);
         } else if (c == "FIND") {
             int x; ss >> x; 
             if (find(x, b)) { cout << "YES\n"; } else { cout << "NO\n"; }
@@ -221,6 +284,9 @@ int main(int argc, char *argv[]) {
         } else if (c  == "RANGE") {
             int x, y; ss >> x >> y; cout << range(x, y, b) << "\n";
         }
+        cerr << "---after request: " << l << ":---\n";
+        printnode(b.root, 2);
+        cerr << "---\n";
     }
     return 0;
 }
