717c8fff6f5bf4f31f40bda9b965a17013406242
Siddharth Bhat <siddu.druid@gmail.com>
2020-03-25 15:07:26 +0530

    add pages

--- gametheory1/solve.py
+++ gametheory1/solve.py
@@ -5,20 +5,40 @@ import pulp
 import numpy as np
 import itertools
 
-assert (len(sys.argv) == 3)
-g = gambit.Game.read_game(sys.argv[1])
 
-# number of strategies for each player
-nstrats = [len(p.strategies) for p in g.players]
-# table of outcomes: #strat(P1) x #strat(P2) x .. x #strat(Pn) -> |Players|
-# table of outcomes: #strat(P1) x #strat(P2) x .. x #strat(Pn) x |Players|
-outcomes = np.empty(nstrats + [len(g.players)])
+def mk_outcomes_array(game):
+    # number of strategies for each player
+    nstrats = [len(p.strategies) for p in game.players]
+    # table of outcomes: #strat(P1) x #strat(P2) x .. x #strat(Pn) -> |Players|
+    # table of outcomes: |Players| x #strat(P1) x #strat(P2) x .. x #strat(Pn)
+    outcomes = np.empty([len(game.players)] + nstrats)
 
-print("nstrats: %s" % nstrats)
+    for strategy_profile in itertools.product(*[range(player_nstrats) for player_nstrats in nstrats]):
+        for p in range(len(game.players)):
+            outcomes[p][strategy_profile] =  game[strategy_profile][p]
+    return outcomes
 
-for strategy_profile in itertools.product(*[range(pi_nstrats) for pi_nstrats in nstrats]):
-    outcomes[strategy_profile] =  list(g[strategy_profile])
-    print("outcomes[%s] = %s" % (strategy_profile, outcomes[strategy_profile]))
+def print_outcomes(os):
+    nstrats = os.shape[1:]
+    for strategy_profile in itertools.product(*[range(player_nstrats) for player_nstrats in nstrats]):
+        # index array as x[..., strategy_profile]
+        print("outcomes[strat=%s] = %s" % (strategy_profile, os[(Ellipsis,) + strategy_profile]))
 
-print("outcomes:\n%s" % outcomes)
+
+def iterate_strong_dominance(os, playerid): 
+    # assert isinstance(os, np.array)
+    assert isinstance(playerid, int)
+
+    print(os[playerid])
+
+
+def calc_strong_dominance(g):
+    os = mk_outcomes_array(g)
+    print_outcomes(os)
+    iterate_strong_dominance(os, 0)
+
+if __name__ == "__main__":
+    assert (len(sys.argv) == 3)
+    g = gambit.Game.read_game(sys.argv[1])
+    calc_strong_dominance(g)
 
