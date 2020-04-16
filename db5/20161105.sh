4979b0b19355f45e5a7dc3df818fd3c6445e3c5e
Siddharth Bhat <siddu.druid@gmail.com>
2020-04-16 09:32:16 +0530

    add DB

--- db5/20161105.sh
+++ db5/20161105.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "$#" -eq 1 ]; then
-    python3 20161105_2.py  $@ # recovery
+    python3 20161105_2.py  $@ > 20161105_2.txt  # recovery >
 fi
 
 if [ "$#" -eq 2 ]; then
-    python3 20161105_1.py  $@ # logging
+    python3 20161105_1.py $@ > 20161105_1.txt  # logging
 fi
